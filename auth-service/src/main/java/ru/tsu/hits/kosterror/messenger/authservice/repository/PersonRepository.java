4c2f7c5d40b3f6490af46afa7d9331ed775d15a2
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-03-27 19:05:16 +0700

    Добавил PersonDetails и PersonDetailsService.

--- auth-service/src/main/java/ru/tsu/hits/kosterror/messenger/authservice/repository/PersonRepository.java
+++ auth-service/src/main/java/ru/tsu/hits/kosterror/messenger/authservice/repository/PersonRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.tsu.hits.kosterror.messenger.authservice.entity.Person;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, UUID> {
+
+    Optional<Person> findByLogin(String login);
+
 }
