12f7fc27e026da9ed95f57c776e0d4783540fa1e
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-06 23:32:33 +0700

    Добавил javadoc на на FriendsServiceApplication.

--- friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/FriendsServiceApplication.java
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/FriendsServiceApplication.java
@@ -3,9 +3,17 @@ package ru.tsu.hits.kosterror.messenger.friendsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс микросервиса.
+ */
 @SpringBootApplication
 public class FriendsServiceApplication {
 
+    /**
+     * Главный метод.
+     *
+     * @param args параметры.
+     */
     public static void main(String[] args) {
         SpringApplication.run(FriendsServiceApplication.class, args);
     }
