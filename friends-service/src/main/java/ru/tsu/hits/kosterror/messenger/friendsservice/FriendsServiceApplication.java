a8ab384f9c856741c9eb121abebd93f5c0f16c69
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-06 23:26:23 +0700

    Инициализировал модуль friends-service

---
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/FriendsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.kosterror.messenger.friendsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsServiceApplication.class, args);
+    }
+
+}
