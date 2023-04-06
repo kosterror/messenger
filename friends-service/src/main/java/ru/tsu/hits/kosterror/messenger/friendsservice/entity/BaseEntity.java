9b9d398ba2a05ed7b55141bbbcc90050184184a0
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-06 23:38:34 +0700

    Добавил BaseEntity.

---
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/entity/BaseEntity.java
@@ -0,0 +1,39 @@
+package ru.tsu.hits.kosterror.messenger.friendsservice.entity;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Класс, представляющий общую информацию всех сущностей для БД.
+ */
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    protected UUID id;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    protected LocalDateTime updatedAt;
+
+}
