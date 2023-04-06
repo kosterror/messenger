424535858d058ad7ae2e95468bad834b28f2c8d7
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-06 16:56:22 +0700

    Добавил бины для кодировщика и декодировщика токенов.

---
+++ auth-service/src/main/java/ru/tsu/hits/kosterror/messenger/authservice/config/JwtEncoderBean.java
@@ -0,0 +1,31 @@
+package ru.tsu.hits.kosterror.messenger.authservice.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+/**
+ * Класс для создания бина кодировщика токенов.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class JwtEncoderBean {
+
+    private final RsaKeyProperties rsaKeys;
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.getPublicKey()).privateKey(rsaKeys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+}
