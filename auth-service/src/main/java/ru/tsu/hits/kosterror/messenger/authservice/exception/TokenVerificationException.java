e9a6c5a203fcd306f5caf55bb37df723010bfedd
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-06 22:43:12 +0700

    Удалил TokenVerificationException, JwtService и его реализацию.

--- auth-service/src/main/java/ru/tsu/hits/kosterror/messenger/authservice/exception/TokenVerificationException.java
+++
@@ -1,9 +0,0 @@
-package ru.tsu.hits.kosterror.messenger.authservice.exception;
-
-public class TokenVerificationException extends Exception {
-
-    public TokenVerificationException(String message) {
-        super(message);
-    }
-
-}
