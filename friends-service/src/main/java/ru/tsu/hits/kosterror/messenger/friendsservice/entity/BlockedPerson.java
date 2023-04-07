455b2c557a6da23e6f508f76b880b6e61a5af219
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-07 22:42:47 +0700

    Добавил сущность заблокированного пользователя и миграцию для него.

---
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/entity/BlockedPerson.java
@@ -0,0 +1,40 @@
+package ru.tsu.hits.kosterror.messenger.friendsservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Класс, представляющий собой заблокированного пользователя
+ */
+@Entity
+@Table(name = "blocked_person")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlockedPerson extends BaseEntity {
+
+    @Column(name = "owner_id")
+    private UUID ownerId;
+
+    @Column(name = "member_id")
+    private UUID memberId;
+
+    @Column(name = "member_full_name")
+    private String memberFullName;
+
+    @Column(name = "date_of_deletion")
+    private LocalDateTime deleteDate;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+}
---
+++ friends-service/src/main/resources/migrations/V2__blocked_person.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blocked_person
+(
+    id               UUID         NOT NULL PRIMARY KEY,
+    created_at       TIMESTAMP,
+    updated_at       TIMESTAMP,
+    owner_id         UUID         NOT NULL,
+    member_id        UUID         NOT NULL,
+    member_full_name VARCHAR(255) NOT NULL,
+    date_of_deletion TIMESTAMP,
+    is_deleted       BOOLEAN      NOT NULL
+);
