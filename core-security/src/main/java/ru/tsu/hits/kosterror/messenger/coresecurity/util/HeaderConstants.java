7deade3701dc6d59dd2eea7179b9d9c01af98bc7
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-09 22:07:30 +0700

    Добавил класс с константами используемых значений хэдеров.

---
+++ core-security/src/main/java/ru/tsu/hits/kosterror/messenger/coresecurity/util/HeaderConstants.java
@@ -0,0 +1,15 @@
+package ru.tsu.hits.kosterror.messenger.coresecurity.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Класс с константами для заголовков запросов.
+ */
+@UtilityClass
+public class HeaderConstants {
+
+    public static final String JWT = "Authorization";
+
+    public static final String API_KEY = "API_KEY";
+
+}
