e6476d4129c828f8b1611fbc4e717091ea161b47
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-10 12:27:56 +0700

    Добавил сервис для генерации JWT токена.

---
+++ core-security/src/main/java/ru/tsu/hits/kosterror/messenger/coresecurity/service/jwt/encoder/JwtServiceEncoderServiceImpl.java
@@ -0,0 +1,53 @@
+package ru.tsu.hits.kosterror.messenger.coresecurity.service.jwt.encoder;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.tsu.hits.kosterror.messenger.coresecurity.config.SecurityProperties;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
+
+import static ru.tsu.hits.kosterror.messenger.coresecurity.util.Constants.*;
+
+/**
+ * Сервис для генерации токенов, реализующий интерфейс {@link JwtServiceEncoder}.
+ */
+@Service
+@RequiredArgsConstructor
+public class JwtServiceEncoderServiceImpl implements JwtServiceEncoder {
+
+    private final SecurityProperties securityProperties;
+
+    @Override
+    public String generateToken(String login, UUID id, String email, String fullName) {
+        Key key = Keys.hmacShaKeyFor(securityProperties.getJwtToken().getSecretKey().getBytes(StandardCharsets.UTF_8));
+        Date issuedAt = Date.from(Instant.now());
+        Date expiresAt = Date.from(
+                Instant.now()
+                        .plus(
+                                securityProperties.getJwtToken().getExpirationMinutes(),
+                                ChronoUnit.MINUTES
+                        )
+        );
+
+        return Jwts
+                .builder()
+                .setIssuer(ISSUER)
+                .setSubject(login)
+                .claim(CLAIM_ID, id.toString())
+                .claim(CLAIM_EMAIL, email)
+                .claim(CLAIM_FULL_NAME, fullName)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiresAt)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+}
