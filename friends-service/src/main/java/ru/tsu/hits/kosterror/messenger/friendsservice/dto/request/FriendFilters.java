cf7681a10e73bf222d59e063168bbe68bddfb2a4
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-11 20:49:54 +0700

    Добавил DTO с параметрами фильтрации друзей.

---
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/dto/request/FriendFilters.java
@@ -0,0 +1,17 @@
+package ru.tsu.hits.kosterror.messenger.friendsservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO для фильтрации друзей.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FriendFilters {
+
+    private String friendFullName;
+
+}
