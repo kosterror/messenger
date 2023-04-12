91df7b40d33d0f8738d207d5eef660f5ee46f6c5
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-12 23:25:43 +0700

    Инициализировал модуль gateway-api.

---
+++ gateway-api/src/main/java/ru/tsu/hits/kosterror/messenger/gatewayapi/GatewayApiApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.kosterror.messenger.gatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApiApplication.class, args);
+    }
+
+}
