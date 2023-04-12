0464ba5d90b2ef7bedcc40eaa8052f1d3889b1db
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-12 19:42:26 +0700

    Добавил Api-Key в сваггер.

--- friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/config/SwaggerConfig.java
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/config/SwaggerConfig.java
@@ -1,14 +1,15 @@
 package ru.tsu.hits.kosterror.messenger.friendsservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
+import ru.tsu.hits.kosterror.messenger.coresecurity.util.Constants;
 
 /**
- * Класс конфигурации для свагера, который позволяет использовать в нем возможность
- * добавления токена в заголовки запросов.
+ * Класс конфигурации для сваггера
  */
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "messenger", version = "v1"))
@@ -17,6 +18,12 @@ import org.springframework.context.annotation.Configuration;
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "Bearer"
+)
+@SecurityScheme(
+        name = Constants.HEADER_API_KEY,
+        type = SecuritySchemeType.APIKEY,
+        scheme = Constants.HEADER_API_KEY,
+        in = SecuritySchemeIn.HEADER
 )
 public class SwaggerConfig {
 }
