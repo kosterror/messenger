89adb1e6abd91cfdf0484e8f4f53ce011254c98e
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-15 10:45:10 +0700

    Добавил DTO для добавления пользователя в черный список.

---
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/dto/CreateBlockedPersonDto.java
@@ -0,0 +1,29 @@
+package ru.tsu.hits.kosterror.messenger.friendsservice.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+/**
+ * DTO для добавления пользователя в черный список.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Объект с информацией для добавления пользователя в черный список.")
+public class CreateBlockedPersonDto {
+
+    @NotNull(message = "Идентификатор не может быть null")
+    @Schema(description = "Идентификатор пользователя, которого нужно заблокировать.")
+    private UUID memberId;
+
+    @NotBlank(message = "ФИО не может быть пустым")
+    @Schema(description = "ФИО пользователя, которого нужно заблокировать.")
+    private String memberFullName;
+
+}
