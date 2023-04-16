d594179e24ddf42cd484fee3c486eb808c54c981
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-04-16 19:20:03 +0700

    Добавил интерфейс AuthIntegrationService и его реализацию для отправки интеграционных запросов в auth-service.

---
+++ friends-service/src/main/java/ru/tsu/hits/kosterror/messenger/friendsservice/service/integrationservice/authservice/AuthIntegrationService.java
@@ -0,0 +1,21 @@
+package ru.tsu.hits.kosterror.messenger.friendsservice.service.integrationservice.authservice;
+
+import org.springframework.http.ResponseEntity;
+import ru.tsu.hits.kosterror.messenger.core.dto.PersonDto;
+
+import java.util.UUID;
+
+/**
+ * Интерфейс с методами, которые посылают интеграционные запросы в auth-service.
+ */
+public interface AuthIntegrationService {
+
+    /**
+     * Метод для отправки интеграционного запроса на получение информации о профиле пользователя.
+     *
+     * @param personId идентификатор пользователя.
+     * @return информация о пользователе. Если статус код 200, то вернется {@link PersonDto}
+     */
+    ResponseEntity<Object> getPersonInfo(UUID personId);
+
+}
