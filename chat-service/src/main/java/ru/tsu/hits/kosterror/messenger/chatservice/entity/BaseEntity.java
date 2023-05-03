5f4b99238de02d41b5deb4a5a142546f1adb060b
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-03 19:41:10 +0700

    Сконфигурировал application.yaml, добавил зависимости в pom.xml, добавил класс базовой сущности BaseEntity.ё

---
+++ chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package ru.tsu.hits.kosterror.messenger.chatservice.entity;
+
+import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+/**
+ * Класс, представляющий общую информацию всех сущностей для БД.
+ */
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    protected UUID id;
+
+}
