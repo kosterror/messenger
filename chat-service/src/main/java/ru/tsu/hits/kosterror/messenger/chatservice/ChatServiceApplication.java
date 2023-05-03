664117da1e2e14e5d52cc60dbe86102b99a304b8
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-03 19:24:42 +0700

    Поправил pom.xml и добавил javadoc на ChatServiceApplication

--- chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/ChatServiceApplication.java
@@ -3,9 +3,17 @@ package ru.tsu.hits.kosterror.messenger.chatservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Класс с точкой входа в программу.
+ */
 @SpringBootApplication
 public class ChatServiceApplication {
 
+    /**
+     * Метод, который является точкой входа в программу.
+     *
+     * @param args параметры для запуска.
+     */
     public static void main(String[] args) {
         SpringApplication.run(ChatServiceApplication.class, args);
     }
