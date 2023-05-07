a2c8875b9c9543970d94c991dc0a2c1c530cf894
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-07 14:27:23 +0700

    Создал репозиторий для сущности чата.

---
+++ chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/repository/ChatRepository.java
@@ -0,0 +1,11 @@
+package ru.tsu.hits.kosterror.messenger.chatservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.tsu.hits.kosterror.messenger.chatservice.entity.Chat;
+
+import java.util.UUID;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, UUID> {
+}
