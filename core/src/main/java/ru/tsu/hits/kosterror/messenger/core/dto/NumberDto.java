836895ec8fadd56a2930fb6c1c7dba34e8142fd4
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-08 01:06:19 +0700

    Добавил NumberDto, поправил тип значения в BooleanDto (заменил примитив на обертку).

---
+++ core/src/main/java/ru/tsu/hits/kosterror/messenger/core/dto/NumberDto.java
@@ -0,0 +1,14 @@
+package ru.tsu.hits.kosterror.messenger.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NumberDto {
+
+    private Integer value;
+
+}
