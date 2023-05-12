91d1a6a3de45f66f0c1e22b505597dcc6894e11f
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-12 16:12:05 +0700

    Унаследовал NotificationRepository от JpaSpecificationExecutor. Добавил NotificationSpecification.

---
+++ notification-service/src/main/java/ru/tsu/hits/kosterror/messenger/notificationservice/service/specification/NotificationSpecification.java
@@ -0,0 +1,37 @@
+package ru.tsu.hits.kosterror.messenger.notificationservice.service.specification;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.data.jpa.domain.Specification;
+import ru.tsu.hits.kosterror.messenger.notificationservice.entity.Notification;
+import ru.tsu.hits.kosterror.messenger.notificationservice.entity.Notification_;
+import ru.tsu.hits.kosterror.messenger.notificationservice.enumeration.NotificationType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@UtilityClass
+public class NotificationSpecification {
+
+    public static Specification<Notification> messageContainsSubstring(String substring) {
+        return ((notification, query, builder) ->
+                builder.like(
+                        builder.upper(notification.get(Notification_.MESSAGE)),
+                        "%" + substring.toUpperCase() + "%")
+        );
+    }
+
+    public static Specification<Notification> receivedDateTimeBefore(LocalDateTime dateTime) {
+        return (notification, query, builder) ->
+                builder.lessThanOrEqualTo(notification.get(Notification_.RECEIVED_DATE), dateTime);
+    }
+
+    public static Specification<Notification> receivedDateTimeAfter(LocalDateTime dateTime) {
+        return (notification, query, builder) ->
+                builder.greaterThanOrEqualTo(notification.get(Notification_.RECEIVED_DATE), dateTime);
+    }
+
+    public static Specification<Notification> typeIn(List<NotificationType> types) {
+        return ((notification, query, builder) -> builder.in(notification.get(Notification_.TYPE)).value(types));
+    }
+
+}
