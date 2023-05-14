74cff8763cc33cb6509a7dcfe106bd0e1aa4c4c5
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-14 19:07:09 +0700

    Добавил тип чата в ChatDto.

--- chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/dto/ChatDto.java
+++ chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/dto/ChatDto.java
@@ -3,6 +3,7 @@ package ru.tsu.hits.kosterror.messenger.chatservice.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.tsu.hits.kosterror.messenger.chatservice.enumeration.ChatType;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ChatDto {
     private UUID id;
+    private ChatType type;
     private String name;
     private UUID avatarId;
     private UUID adminId;
