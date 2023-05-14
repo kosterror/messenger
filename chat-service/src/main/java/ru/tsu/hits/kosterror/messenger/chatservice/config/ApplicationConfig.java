4b5ae717747f84d04da3a60d6eb431ba40c23a35
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-14 19:07:54 +0700

    Добавил бины для интеграционных запросов в friends-service из chat-service.

---
+++ chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/config/ApplicationConfig.java
@@ -0,0 +1,47 @@
+package ru.tsu.hits.kosterror.messenger.chatservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import ru.tsu.hits.kosterror.messenger.core.integration.common.CommonIntegrationService;
+import ru.tsu.hits.kosterror.messenger.core.integration.common.CommonIntegrationServiceImpl;
+import ru.tsu.hits.kosterror.messenger.core.integration.friends.FriendIntegrationService;
+import ru.tsu.hits.kosterror.messenger.core.integration.friends.FriendIntegrationServiceImpl;
+
+/**
+ * Конфигурационный класс сервиса.
+ */
+@Configuration
+public class ApplicationConfig {
+
+    /**
+     * Создает бин {@link RestTemplate}.
+     *
+     * @return бин {@link RestTemplate}.
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * Метод для создания бина {@link CommonIntegrationService}.
+     *
+     * @return бин {@link CommonIntegrationService}.
+     */
+    @Bean
+    public CommonIntegrationService commonIntegrationService() {
+        return new CommonIntegrationServiceImpl();
+    }
+
+    /**
+     * Метод для создания бина {@link FriendIntegrationService}.
+     *
+     * @return бин {@link CommonIntegrationService}.
+     */
+    @Bean
+    public FriendIntegrationService friendIntegrationService() {
+        return new FriendIntegrationServiceImpl(restTemplate(), commonIntegrationService());
+    }
+
+}
