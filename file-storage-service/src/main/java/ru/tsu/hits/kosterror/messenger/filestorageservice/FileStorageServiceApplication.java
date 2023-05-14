d125fe48f15bf46cc05d1237d2d10fb19b618a17
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-15 03:54:45 +0700

    Добавил конфиг для сваггера и защиту эндпоинтов с помощью core-security.

--- file-storage-service/src/main/java/ru/tsu/hits/kosterror/messenger/filestorageservice/FileStorageServiceApplication.java
+++ file-storage-service/src/main/java/ru/tsu/hits/kosterror/messenger/filestorageservice/FileStorageServiceApplication.java
@@ -3,9 +3,13 @@ package ru.tsu.hits.kosterror.messenger.filestorageservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import ru.tsu.hits.kosterror.messenger.core.requestlogger.ImportRequestLoggingFilter;
+import ru.tsu.hits.kosterror.messenger.coresecurity.ImportCoreSecurityApplication;
 
 @ConfigurationPropertiesScan("ru.tsu.hits.kosterror.messenger.filestorageservice")
 @SpringBootApplication
+@ImportCoreSecurityApplication
+@ImportRequestLoggingFilter
 public class FileStorageServiceApplication {
 
     public static void main(String[] args) {
