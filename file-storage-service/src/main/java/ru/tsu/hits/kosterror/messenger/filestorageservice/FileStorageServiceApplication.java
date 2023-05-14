9703fcb6cdba30c022c30b2f7bb15e04bc28791c
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-15 01:26:28 +0700

    Добавил зависимости и сконфигурировал minio.

--- file-storage-service/src/main/java/ru/tsu/hits/kosterror/messenger/filestorageservice/FileStorageServiceApplication.java
+++ file-storage-service/src/main/java/ru/tsu/hits/kosterror/messenger/filestorageservice/FileStorageServiceApplication.java
@@ -2,7 +2,9 @@ package ru.tsu.hits.kosterror.messenger.filestorageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan("ru.tsu.hits.kosterror.messenger.filestorageservice")
 @SpringBootApplication
 public class FileStorageServiceApplication {
 
