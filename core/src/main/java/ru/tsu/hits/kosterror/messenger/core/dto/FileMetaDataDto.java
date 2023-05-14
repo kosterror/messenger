244b5ff1e2a96d19c09c9350c8ad6c39a9e6a4f5
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-15 02:28:23 +0700

    Добавил FileMetaDataDto.

---
+++ core/src/main/java/ru/tsu/hits/kosterror/messenger/core/dto/FileMetaDataDto.java
@@ -0,0 +1,33 @@
+package ru.tsu.hits.kosterror.messenger.core.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Класс, который представляет собой метаинформацию о файле.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Объект для хранения метаинформации файла из file-storage-service.")
+public class FileMetaDataDto {
+
+    @Schema(description = "Идентификатор файла.")
+    private UUID fileId;
+
+    @Schema(description = "Название файла. Остается таким же, каким было при загрузке.",
+            example = "Файл с таким названием.txt")
+    private String name;
+
+    @Schema(description = "Идентификатор пользователя, который загрузил этот файл.")
+    private UUID authorId;
+
+    @Schema(description = "Дата и время загрузки файла.")
+    private LocalDateTime uploadDateTime;
+
+}
