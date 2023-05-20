348e44f10de4d698fa0a854dfaac27b755d52183
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-21 00:00:31 +0700

    Добавил отправку уведомления об аутентфикации.

---
+++ auth-service/src/main/java/ru/tsu/hits/kosterror/messenger/authservice/util/constant/RabbitMQBindings.java
@@ -0,0 +1,8 @@
+package ru.tsu.hits.kosterror.messenger.authservice.util.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RabbitMQBindings {
+    public static final String CREATE_NOTIFICATION_OUT = "createNotification-out-0";
+}
