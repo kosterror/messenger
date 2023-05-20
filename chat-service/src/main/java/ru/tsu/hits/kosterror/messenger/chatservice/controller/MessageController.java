b0a8b7fa88357cebbcbd54ab62f1cd9a5e1e3113
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-05-20 13:53:20 +0700

    Поменял роутинг в chat-service.

--- chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/controller/MessageController.java
+++ chat-service/src/main/java/ru/tsu/hits/kosterror/messenger/chatservice/controller/MessageController.java
@@ -24,9 +24,12 @@ public class MessageController {
     private final MessageService messageService;
 
 
-    @PostMapping("/group/message")
+    @PostMapping("/message/send")
     @Operation(
-            summary = "Отправить сообщение в беседу.",
+            summary = "Отправить сообщение в чат.",
+            description = "Сообщение может отправиться в личный чат и в беседу." +
+                    "Чтобы начать личный чат нужно сперва использовать запроса на отправку " +
+                    "сообщения в личный диалог (\"Отправить сообщение в личный диалог.\").",
             security = @SecurityRequirement(name = "bearerAuth")
     )
     public void sendMessageToGroupChat(Authentication auth,
@@ -34,7 +37,7 @@ public class MessageController {
         messageService.sendMessageToGroupChat(extractId(auth), dto);
     }
 
-    @PostMapping("/private/message")
+    @PostMapping("/private/message/send")
     @Operation(
             summary = "Отправить сообщение в личный диалог.",
             security = @SecurityRequirement(name = "bearerAuth")
@@ -44,7 +47,7 @@ public class MessageController {
         messageService.sendMessageToPrivateChat(extractId(auth), dto);
     }
 
-    @GetMapping("/{chatId}/messages")
+    @GetMapping("/{chatId}/message")
     @Operation(
             summary = "Получить сообщения чата.",
             security = @SecurityRequirement(name = "bearerAuth")
@@ -54,7 +57,7 @@ public class MessageController {
         return messageService.getChatMessages(extractId(auth), chatId);
     }
 
-    @GetMapping("/search-messages")
+    @GetMapping("/search-message")
     @Operation(
             summary = "Поиск сообщения.",
             security = @SecurityRequirement(name = "bearerAuth")
